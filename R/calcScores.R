6b51710b7edb2358b6191cc50e7d023d7f842460
Shannon Kwan <s.kwan@victorchang.edu.au>
2021-03-02 11:16:52 +1100

    Enforce SIMPLIFY

--- R/calcScores.R
+++ R/calcScores.R
@@ -4,14 +4,14 @@ getFile <- function(inFile, inRecord){
     print(inData)
     inData[,PGS_RECORD_ID := inRecord]
     #inData[,subject_type := "diseased"]
-    return(inData)
+    return(data.table(inData))
   } else {
     return(NULL)
   }
 }
 
 getAggDf <- function(inFiles, inRecords){
-  totalSamples <- data.table::rbindlist(mapply(getFile, inFiles, inRecords))
+  totalSamples <- data.table::rbindlist(mapply(getFile, inFiles, inRecords, SIMPLIFY=FALSE))
   aggSamples <- totalSamples[,sum(as.numeric(SCORESUM)), by=.(IID, PGS_RECORD_ID)]
 
   setnames(aggSamples, "V1", "PRS")
